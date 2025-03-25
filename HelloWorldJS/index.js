1d51268fff87c342640166d655b24581cf506675
TheAnhDev369 <theanhdev369@gmail.com>
2025-03-25 08:23:44 +0700

    1. Hello World with JS

---
+++ HelloWorldJS/index.js
@@ -0,0 +1,12 @@
+alert("Hello World !");
+
+let js = "amazing";
+if (js === "amazing") alert("JavaScript is FUN");
+
+js = "boring";
+if (js === "amazing") alert("JavaScript is FUN");
+
+// console.log(40 + 8 + 23 - 10);   61
+
+//  C1: Sử dụng Js Console để viết code JS trực tiếp trên đó
+//  C2: Sử dụng thể script src để liên kết file js viết code đến file html và chạy Live Server
