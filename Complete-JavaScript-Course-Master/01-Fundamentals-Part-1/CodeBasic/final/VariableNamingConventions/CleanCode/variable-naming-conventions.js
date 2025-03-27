69c18ebfc43d8c76f3ede12ed4b897695201b81d
TheAnhDev369 <theanhdev369@gmail.com>
2025-03-27 08:48:23 +0700

    Lesson 01 - Fundamentals - Part 1.3 - Variable Naming Conventions in JavaScript - Update

--- Complete-JavaScript-Course-Master/01-Fundamentals-Part-1/CodeBasic/final/VariableNamingConventions/CleanCode/variable-naming-conventions.js
+++ Complete-JavaScript-Course-Master/01-Fundamentals-Part-1/CodeBasic/final/VariableNamingConventions/CleanCode/variable-naming-conventions.js
@@ -1,12 +1,12 @@
-//  Ví dụ về cáh đặt tên các biến
+// QUY TẮC MỀM //
+
 let myFullName = "Nguyễn Thế Anh";
 let my_id = 1;
 let MyAdress = "Hà Nội";
-
 console.table({
-  myFullName, // camelCase
-  my_id, // kebab case
-  MyAdress, // pascal case
+  myFullName,
+  my_id,
+  MyAdress,
 });
 
 //  1.  Sử dụng camelCase cho BIẾN VÀ HÀM(*)
@@ -39,16 +39,17 @@ console.log(myCar.brand);
 console.log(myCar.model);
 console.log(myCar.color);
 
-//  HẰNG SỐ
+// 3.   Sử dụng UPPER_CASE cho hằng số
 const MAX_LENGHT = 100;
 console.log(MAX_LENGHT);
 
-//  Đặt tên biến không được có dấu - hoặc số ở đầu
-// let 1years = 2020; Sai
-// let year-1 = 2021; Sai
-let year_1 = 2021; // Đúng
-let Year1 = 2023; // Đúng
-let yearOne = 2024; // Đúng
+// QUY TẮC CỨNG //
+// 1.  Không được bắt đầu bằng số hoặc có dấu -
+// let 1years = 2020;
+// let year-1 = 2021;
+let year_1 = 2021;
+let Year1 = 2023;
+let yearOne = 2024;
 
 console.table({
   year_1,
@@ -59,30 +60,44 @@ console.table({
 let Year2025 = 2025;
 console.table(Year2025);
 
-//  Đúng các cú pháp quy chuẩn khi đặt tên biến
-let a_z = "a-z"; // Đúng
-let AZ = "A-Z"; // Đúng,
-let Number1 = 1; //  Đúng
-let $variables = "Variables"; // Đúng
+// 2.  Chỉ được chứa các chữ cái (a-z,A-Z), số (0-9), dấu gạch dưới _ hoặc ký tự $.
+let a_z = "a-z";
+let AZ = "A-Z";
+let Number1 = 1;
+let $variables = "Variables";
 console.table({
   a_z,
   AZ,
   Number1,
   $variables,
 });
-
-// Biến sai do trùng Reserved Keywords (Từ khoá JS)
+// 3.  Không được trùng với từ khoá Js (Reserved Keywords)
 // let let = 100;
 // var var = "Variables";
 // const const = "DEV";
+// let new = 27;
+// let function = 27;
 
-//  Có phân biệt chữ hoa, chữ thường ở biến
+//  Khắc phục
+let _let = 100;
+var $var = "Variables";
+const _const = "DEV";
+let $new = 27;
+let _function = 27;
+console.table({
+  _let,
+  $var,
+  _const,
+  $new,
+  _function,
+});
+// 4.   Có phân biệt chữ hoa và chữ thường (case-sensitive)
 let myVar = 20;
 let MyVar = 10;
-console.log(myVar); // 20
-console.log(MyVar); // 10
+console.log(myVar);
+console.log(MyVar);
 
-//  Đặt tên biến không được chứa dấu cách
+//  5.  Không sử dụng dấu cách
 // let my variables = "Dev"; // Sai
 let myVariables = "DEV"; // Đúng
 console.log(myVariables);
