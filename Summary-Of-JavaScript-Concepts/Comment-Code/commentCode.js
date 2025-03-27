601d928ba23487e7530a9a243233e1a1910784c3
TheAnhDev369 <theanhdev369@gmail.com>
2025-03-27 11:34:47 +0700

    Comment Code in Javascript

---
+++ Summary-Of-JavaScript-Concepts/Comment-Code/commentCode.js
@@ -0,0 +1,21 @@
+//  Trong lập trình, chúng ta sử dụng các chú thích (comment) để đến mô tả và viết mã theo nghĩa đen, hoặc
+//  huỷ kích hoạt mã code mà không xoá nó
+//  VD: Ta sử dụng // và sau đó có thể mô tả mã này đang làm gì, chức năng của nó như thế nào ?
+
+//  Variable name conventions
+//  let myAddress = "Ha Noi";
+//  JS sẽ hoàn toàn bỏ bất cứ thứ gì là bình luận
+//  Điều này thực sự hữu ích khi phân chia hoặc viết mã thành các phần khác nhau và
+//  để giải thích những phần mã khác nhau đang thực sự làm gì trong mã của chúng ta
+
+//  Về cơ bản, đây chỉ là 1 ví dụ minh hoạ nhận xét rất hữu ích.
+//  Một ứng dụng khác của commnet chỉ đơn giản bình luận để không thực thi một số mã vì 1 số lý do như mã lỗi hoặc muốn đóng lại thay vì xoá để sửa lỗi sau này .
+
+//  Để comment 1 dòng, ta có thể sử dụng Ctrl + / hoặc đơn giản là 2 dấu // slash slash đê cmt.
+//  Để comment nhiều dòng, ta có thể sử dụng slash ** slash để cmt nhiều dòng
+
+//  Ví dụ 1, sử dụng // hoặc Ctrl + / để cmt, gnoaif ra có thể bỏ cmt bằng cách nhấn thêm lần nữa Ctrl + /
+/**
+ * Đây là comment nhiều dòng, để bình luận, mô tả chi tiết mã 1 cách dài dòng,chi tiết
+ * Với mỗi đầu dòng là 1 dấu *
+ */
