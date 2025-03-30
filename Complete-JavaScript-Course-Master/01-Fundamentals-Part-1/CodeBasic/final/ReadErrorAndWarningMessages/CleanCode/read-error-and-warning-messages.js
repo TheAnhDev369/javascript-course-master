6daff0b4c6efb76485baf9b9f7508cb3b50a2a8d
TheAnhDev369 <theanhdev369@gmail.com>
2025-03-30 16:25:04 +0700

    Add md PDF-Coding Challenges

--- Complete-JavaScript-Course-Master/01-Fundamentals-Part-1/CodeBasic/final/ReadErrorAndWarningMessages/CleanCode/read-error-and-warning-messages.js
+++ Complete-JavaScript-Course-Master/01-Fundamentals-Part-1/CodeBasic/final/ReadErrorAndWarningMessages/CleanCode/read-error-and-warning-messages.js
@@ -22,3 +22,16 @@ let x = 100; //  Number
 console.log(h);
 //  -   Uncaught ReferenceError: h is not defined
 //  -   Lỗi tham chiếu xảy ra khi biến chưa được khai báo hay định nghĩa trước khi sử dụng.
+
+// Ví dụ 5:
+// Năm sinh của tôi là 2004
+const birthDay = 2004;
+//  Nếu sau đó chúng ta cố gắng gán lại cho cái biến đó giá trị khác (giả sử như 2003)
+//  thì nó sẽ không hoạt động.
+//  Lỗi : Uncaught TypeError: Assignment to constant variable. at ...
+//  Lỗi này xảy ra vì birthDay được khai báo bằng const,
+//  tức là nó là một hằng số (constant) và không thể thay đổi sau khi đã được gán giá trị ban đầu.
+
+//  Ví dụ 6:
+// const job;
+// Uncaught SyntaxError: Missing initializer in const declaration ...
