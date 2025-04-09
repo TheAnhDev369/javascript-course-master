0b87b244da21213e3bb530ab595117b69d1b1387
TheAnhDev369 <theanhdev369@gmail.com>
2025-04-09 14:25:49 +0700

    Lesson01 - Fundamentals - Part 1.1 Update LinkJavaScript P1

---
+++ Complete-JavaScript-Course-Master/01-Fundamentals-Part-1/BasicCodeWithMeAndChatGPT/UnderRevisionAndUpdateCode/LinkJavaScript/CleanCode/external-link-js-1.js
@@ -0,0 +1,7 @@
+let js1 = "boring";
+if ((js1 = "boring")) console.log("JS is so boring !");
+
+let js2 = "amazing";
+if (js2 === "amazing") alert("JavaScript is verry FUN!");
+
+console.log(40 + 8 + 23 - 10);
